a54d3bd5fe91ccd3e2e8295463c4d7c034dc6606
Saloni Shah <2023sshah@tjhsst.edu>
2024-08-21 17:26:10 -0400

    added background changes for bottom half

--- components/PastEventsGrid.vue
+++ components/PastEventsGrid.vue
@@ -1,11 +1,28 @@
 <template>
-  <div class="section-container">
-    <div class="past-grid-container">
-      <div class="past-grid">
-        <EventCard id="cowboy-icon" />
-        <template v-for="year in years" :key="year">
-          <EventCard :year="year" :theme="themes[year]" />
-        </template>
+  <div class="event-text-only">
+    <div class="event-theme">Explore The Past Ten Years of Bitcamp</div>
+  </div>
+  <div class="gradient">
+    <div class="section-container">
+      <div class="past-grid-container">
+        <div class="past-grid">
+          <template v-for="year in years" :key="year">
+            <EventCard :year="year" :theme="themes[year]" />
+          </template>
+        </div>
+      </div>
+      <div class="footer">
+        <div class="floatie-text past-grid-container">
+          <div class="links">
+            <a href="http://mlh.io/code-of-conduct">MLH Code of Conduct</a> |
+            <a href="/brand.pdf">Brand Guidelines</a> |
+            <a href="mailto:hello@bit.camp">Contact Us</a>
+            <div class="copyright">
+              Copyright &copy; 2019 - 2024 Bitcamp. All Rights Reserved.
+            </div>
+          </div>
+          <img class="elvis" src="../assets/img/images/elvis.svg" alt="elvis">
+        </div>
       </div>
     </div>
   </div>
@@ -37,10 +54,85 @@ export default {
 </script>
 
 <style scoped>
-#cowboy-icon {
-  background-image: url('../assets/img/images/cowboy-hat.svg');
-  background-repeat: no-repeat;
-  background-size: 50% 50% !important;
+
+.elvis {
+  width: 125px;
+  height: 125px;
+  transform: rotate(4.426deg);
+}
+
+.event-text {
+  position: absolute;
+  bottom: 0;
+  padding: 0px 0px 20px 25px;
+}
+
+.event-text-only {
+  margin: 0px 60px 0 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.event-text h3 {
+  font-family: Aleo;
+  font-weight: 700;
+  font-size: 4rem;
+}
+
+.event-theme {
+  font-family: Aleo;
+  font-weight: bold;
+  font-size: 4rem;
+
+  text-align: center;
+  padding-top: 3rem;
+  padding-bottom: 4rem;
+
+  color: #FFC08E;
+}
+
+.footer {
+  position: relative;
+  box-sizing: border-box;
+
+  height: 15vw;
+  width: 100vw;
+  overflow: hidden;
+}
+
+.links a {
+  text-decoration: underline;
+}
+.floatie-text {
+  position: absolute;
+  text-align: center;
+  margin: auto;
+  left: 0;
+  right: 0;
+  bottom: 5vw;
+  font-size: 1.5vw;
+}
+
+@media (max-width: 976px) {
+  .floatie-text {
+    font-size: 2vw;
+  }
+}
+
+@media (max-width: 768px) {
+  .floatie-text {
+    font-size: 2.5vw;
+  }
+  .elvis {
+    width: 75px;
+    height: 75px;
+  }
+}
+
+.gradient {
+  background: linear-gradient(180deg, #13192E 0%, #393D64 60.5%, #656080 81.83%, #BD9290 95.5%);
 }
 
 .section-container {
